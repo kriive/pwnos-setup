61e0aa27a7ac2d3270f774662249f2e4bd4725fa
Manuel Romei <manuel.romei@outlook.it>
2023-08-20 03:26:04 +0200

    fix: add missing packages and comment for now gdb-multiarch

--- install-packages.sh
+++ install-packages.sh
@@ -4,4 +4,4 @@
 ansible-galaxy collection install -r requirements.yml
 
 # Install everything!
-ansible-playbook -K pwnos-setup.yml -i "localhost:22222,"
+ansible-playbook -K pwnos-setup.yml -i "localhost:22222," -u kriive --extra-vars "ansible_user=kriive ansible_port=22222"
